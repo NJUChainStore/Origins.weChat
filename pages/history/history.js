f7066db766faf06db55607e1780108d0a293a61d
julia98 <925583000@qq.com>
2018-05-15 17:17:45 +0800

    fix

--- pages/history/history.js
+++ pages/history/history.js
@@ -1,29 +1,61 @@
 const app = getApp()
 Page({
   data: {
-    ProductShortList: [
+    loading: true,
+
+    productShortList: [
       /*{
         productId: "光明牛奶",
-        producedDate: "2018.5.13 12:21:39"
+        produceDate: "2018.5.13 12:21:39"
       },
       {
         productId: "放心牛奶",
-        producedDate: "2018.5.12 22:44:02"
+        produceDate: "2018.5.12 22:44:02"
       },
       {
         productId: "进口牛奶",
-        producedDate: "2018.5.13 08:55:07"
+        produceDate: "2018.5.13 08:55:07"
       }*/
     ]
   },
   object: {
     productId: '',
-    producedDate: ''
+    produceDate: ''
   },
   onLoad: function (options) {
+    var that = this
+    wx.request({
+      url: 'http://localhost:12494/Product/history',
+      method: 'GET',
+      header: {
+        //'content-type': 'application/json',
+        'Authorization': `Bearer ${app.globalData.token}`
+      },
+      success: function (res) {
+        console.log(res.data)
+        if (res.data != null) {
+          that.setData({
+            productShortList: res.data.productShortList
+          })
+        } else {
+          wx.showToast({
+            title: '当前没有历史查询内容',
+          })
+        }
+      },
+      fail: function (e) {
+        console.log(e)
+      },
+      complete: function () {
+        that.setData({
+          loading: false
+        })
+      }
+    })
   },
 
   onShow: function () {
+    /*
     var that = this
     wx.request({
       url: 'http://localhost:12494/Product/history',
@@ -49,26 +81,23 @@ Page({
       },
       complete: function () {
         //再刷新一遍更新数据
-        that.onLoad()
       }
-    })
+    })*/
     
   },
 
-  requireDetail: function () {
+  requireDetail: function (e) {
+    console.log(e.currentTarget.dataset.productid)
     var that = this
     wx.navigateTo({
-      url: `../detail/detail?productId=${that.data.productId}`
+      url: `../detail/detail?productId=${e.currentTarget.dataset.productid}`
     })
   },
 
-  detail: function () {
-  
-  },
-  delete: function () {
+  delete: function (e) {
     var that = this
     wx.request({
-      url: `http://localhost:12494/Product/history?productId=${that.data.productId}`,
+      url: `http://localhost:12494/Product/history?productId=${e.currentTarget.dataset.productid}`,
       method: 'DELETE',
       header: {
         //'content-type': 'application/json',
